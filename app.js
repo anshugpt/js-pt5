2892ba92fb0666bb66b622b7637845d7df611217
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-10-12 13:40:46 +0530

    docs : JS file add topic objects

---
+++ app.js
@@ -0,0 +1,23 @@
+//* Object Literals
+// used to store keyed collections & complex entities
+// keyed collection --> property ==> (key, value) pair
+// i.e -> Student info = 'name' -> "Anshu", 'age' -> 21, 'marks' -> 70
+// key --> name, age, marks --> we call key because it access the value
+// value --> "Anshu", 21, 70 --> this is value of the respective keys
+// Objects are a collection of properties
+// TODO: Read Docs on MDN about Object Literals
+
+//* syntax :-
+// there is no order to follow like array index
+// if constant then it follow array referance
+// means we can change key & value but can not change memory address
+// generaly we use const in object laterals
+// we can add an array in key & value
+
+// const student = {
+//     name: "Anshu",  
+//     age: 21,        
+//     marks: 70,
+//     uniformColor: ["Blue", "White"]
+// };
+
